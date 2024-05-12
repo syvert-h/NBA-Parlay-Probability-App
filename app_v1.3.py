28a8c8307ad217fe34f876f2adfec34b18bf8f2d
Syvert <syv3rt@gmail.com>
2024-05-13 11:26:20 +1200

    Added pre-requisites for render

--- app_v1.3.py
+++ app_v1.3.py
@@ -14,6 +14,7 @@ import os
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "NBA Parlay Dashboard"
+server = app.server
 
 ##### SETUP CODE #####
 ## AWS S3 Bucket Settings
